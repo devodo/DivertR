72e39f1a26fdb98f7da37cc908e1d0b99258b8f5
David Naylor <devodo@users.noreply.github.com>
2022-08-16 21:13:02 +0100

    Add VerifyAsync helper extensions (#22)

---
+++ src/DivertR/VerifyAsyncExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using DivertR.Record;
+
+namespace DivertR
+{
+    public static class VerifyAsyncExtensions
+    {
+        public static async Task<IVerifySnapshot<Task<TMap>>> VerifyAsync<TMap>(this ICallStream<Task<TMap>> callStream, Action<TMap> visitor)
+        {
+            var verifySnapshot = callStream.Verify();
+            
+            foreach (var callTask in verifySnapshot)
+            {
+                var call = await callTask.ConfigureAwait(false);
+                visitor.Invoke(call);
+            }
+
+            return verifySnapshot;
+        }
+
+        public static async Task<IVerifySnapshot<Task<TMap>>> VerifyAsync<TMap>(this ICallStream<Task<TMap>> callStream, Func<TMap, Task> visitor)
+        {
+            var verifySnapshot = callStream.Verify();
+            
+            foreach (var callTask in verifySnapshot)
+            {
+                var call = await callTask.ConfigureAwait(false);
+                await visitor.Invoke(call).ConfigureAwait(false);
+            }
+
+            return verifySnapshot;
+        }
+    }
+}
