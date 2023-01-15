ace48bdc708f88912bb9f340551edcfb99fa7465
David Naylor <dave.naylor@gmail.com>
2023-01-15 22:19:27 +0000

    Switch to dark mode based on system preference

---
+++ docs/_includes/js/custom.js
@@ -0,0 +1,14 @@
+﻿if (window.matchMedia) {
+    window.matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', event => {
+            if (event.matches) {
+                jtd.setTheme('dark');
+            } else {
+                jtd.setTheme('light');
+            }
+        });
+
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        jtd.setTheme('dark');
+    }
+}
